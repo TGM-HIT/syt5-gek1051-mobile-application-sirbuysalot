c9932dc2ceee4956002a59e6b58dea278ac8eb2e
Sandip Sarana <martin.sarana@gmail.com>
2026-03-14 14:00:00 +0100

    US-04: add product name validation, error handling

--- backend/src/main/java/at/tgm/sirbuysalot/controller/ProductController.java
+++ backend/src/main/java/at/tgm/sirbuysalot/controller/ProductController.java
@@ -2,6 +2,7 @@ package at.tgm.sirbuysalot.controller;
 
 import at.tgm.sirbuysalot.model.Product;
 import at.tgm.sirbuysalot.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +24,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<Product> create(@PathVariable UUID listId, @RequestBody Product product) {
+    public ResponseEntity<Product> create(@PathVariable UUID listId, @Valid @RequestBody Product product) {
         return ResponseEntity.ok(service.create(listId, product));
     }
 
